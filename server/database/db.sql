2ad89001a475a9b0492246a22c3da51af7c28a09
Kenobi17 <matiascorrea420@gmail.com>
2020-10-01 00:08:24 -0300

    Add database folder with db.sql to show the table schema and dbconfig.js to connect the database to the server

---
+++ server/database/db.sql
@@ -0,0 +1,7 @@
+create table users (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
+
+-- Generate mock data with www.mockaroo.com and insert that data into the database
---
+++ server/database/dbconfig.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const db = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWROD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
+
+module.exports = db;
